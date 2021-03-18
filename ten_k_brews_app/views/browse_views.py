ec5d717b8342d7e4005e0eddbe196d52a5f01af8
cjdufault <dufaultconnor@gmail.com>
2021-03-18 11:16:27 -0500

    add list_title to list.html

--- ten_k_brews_app/views/browse_views.py
+++ ten_k_brews_app/views/browse_views.py
@@ -25,26 +25,34 @@ def home(request):
 def browse(request, type_filter):
     # get establishments to list based on user selection
     if type_filter == 'all':
+        list_title = 'All:'
         establishments = Establishment.objects.all().order_by('name')
     elif type_filter == 'breweries':
+        list_title = 'Breweries:'
         establishments = Establishment.objects.filter(type=Establishment.BREWERY).order_by('name')
     elif type_filter == 'wineries':
+        list_title = 'Wineries:'
         establishments = Establishment.objects.filter(type=Establishment.WINERY).order_by('name')
     elif type_filter == 'distilleries':
+        list_title = 'Distilleries:'
         establishments = Establishment.objects.filter(type=Establishment.DISTILLERY).order_by('name')
     elif type_filter == 'cideries':
+        list_title = 'Cideries:'
         establishments = Establishment.objects.filter(type=Establishment.CIDERY).order_by('name')
     else:
         return redirect('home')     # redirect home if type_filter is something unexpected
 
-    return render(request, 'browse_pages/list.html', {'establishments': establishments, 'search_form': search_form})
+    return render(request, 'browse_pages/list.html', {'establishments': establishments, 'list_title': list_title,
+                                                      'search_form': search_form})
 
 
 def search(request):
     search_term = request.GET.get('search_term')
 
     if search_term:     # check that search term is entered
+        list_title = f'Results for "{search_term}":'
         establishments = Establishment.objects.filter(name__icontains=search_term).order_by('name')
-        return render(request, 'browse_pages/list.html', {'establishments': establishments, 'search_form': search_form})
+        return render(request, 'browse_pages/list.html', {'establishments': establishments, 'list_title': list_title,
+                                                          'search_form': search_form})
 
     return redirect('browse', type_filter='all')     # show /browse/all if no search term provided
