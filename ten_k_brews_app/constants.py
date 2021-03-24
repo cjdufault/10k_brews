c3b2cb18798ae4c0bb1640f80ef6aa24ee4ce4bf
cjdufault <dufaultconnor@gmail.com>
2021-03-24 11:36:03 -0500

    adjust mn coordinates in constants.py

--- ten_k_brews_app/constants.py
+++ ten_k_brews_app/constants.py
@@ -1,3 +1,7 @@
-MINNESOTA_COORDINATES = 45.9896587, -94.6113288
-LOCATION_ZOOM_LEVEL = 13
-WIDE_ZOOM_LEVEL = 6
+"""
+Configuration file for constants used elsewhere
+"""
+
+MINNESOTA_COORDINATES = 46.5, -94.0     # coordinates to center map on to put MN in view
+LOCATION_ZOOM_LEVEL = 13                # zoom level for specific locations (close enough for road detail)
+WIDE_ZOOM_LEVEL = 6                     # zoom level for view of all of MN
